d912e4a9493df2cbb69597851def13138ce7fd5b
Fahad Mahmood <fm1592933@gmail.com>
2025-08-24 21:58:11 -0700

    Update js

--- JS/24 Questions/index.js
+++ JS/24 Questions/index.js
@@ -74,3 +74,90 @@
 // }
 // console.log(Str2);
 
+
+
+// Question 4
+
+// function passWord(str) {
+//     let code;
+//     let flag1 = false;
+//     let flag2 = false;
+//     let flag3 = false;
+
+//     for (let index = 0; index < str.length; index++) {
+//         code = str.charCodeAt(index);
+//         if (code >= 65 && code <= 90) {
+//             flag1 = true;
+//         }
+//     }
+//     for (let index = 0; index < str.length; index++) {
+//         code = str.charCodeAt(index);
+//         if (code >= 97 && code <= 122) {
+//             flag2 = true;
+//         }
+//     }
+//     for (const ch of str) {
+//         if (ch >= '0' && ch <= '9') {
+//             flag3 = true;
+//         }
+//     }
+
+//     if (flag1 == true && flag2 == true && flag3 == true) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// let Str = "Abc12";
+// let Str2 = "Abc";
+// let isTrue = true;
+// isTrue = passWord(Str2);
+// if (isTrue == true) {
+//     console.log("OK");
+// } else {
+//     console.log("Invalid");
+// }
+
+
+
+// Question 5
+
+// function SumNum(Arr) {
+//     let result = 0;
+//     for (let index = 0; index < Arr.length; index++) {
+//         if (Arr[index] < 0) {
+//             return result;
+//         }
+//         else {
+//             result += Arr[index];
+//         }
+//     }
+//     return result;
+// }
+
+// let Arr = [1, 2, 3, 4, -5];
+// let result = SumNum(Arr);
+// console.log(result);
+
+
+
+// Question 6
+
+// let vowelsL = ["a", "e", "i", "o", "u"];
+// let vowelsU = ["A", "E", "I", "O", "U"];
+// function CountVowels(str) {
+//     let count = 0;
+//     for (let index = 0; index < str.length; index++) {
+//         for (let i = 0; i < vowelsL.length; i++) {
+//             if (str[index] == vowelsL[i] || str[index] == vowelsU[i]) {
+//                 count++;
+//             }
+//         }
+
+//     }
+//     return count;
+// }
+
+// let str = "Fahad";
+// let count = CountVowels(str);
+// console.log(count);
